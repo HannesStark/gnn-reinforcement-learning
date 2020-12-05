872dc7792777af96a93a49b54f4c89716f020629
Tobias Schmidt <tsbaunatal@gmail.com>
2020-12-05 20:47:12 +0100

    simple training example

---
+++ exploration_stable_baseline/stable_baselines_getting_started.py
@@ -0,0 +1,16 @@
+import gym
+from gym import wrappers
+from stable_baselines3 import A2C
+
+env = gym.make('CartPole-v1')
+
+model = A2C('MlpPolicy', env, verbose=1)
+model.learn(total_timesteps=10000)
+
+obs = env.reset()
+for i in range(1000):
+    action, _state = model.predict(obs, deterministic=True)
+    obs, reward, done, info = env.step(action)
+    env.render()
+    if done:
+        obs = env.reset()
