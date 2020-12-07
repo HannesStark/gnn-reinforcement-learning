91932b8bac8266533ab3983094eb73cb53c848a4
Hannes Stärk <hannes.staerk@gmail.com>
2020-12-07 11:12:37 +0100

    modify actor critic policy

--- exploration_pybullet/explore.py
+++ exploration_pybullet/explore.py
@@ -1,28 +1,17 @@
-import pybullet as p
-from time import sleep
-import pybullet_data
+import gym
+from gym import wrappers
+from stable_baselines3 import A2C
+import pybullet_envs  # register pybullet envs from bullet3
+from stable_baselines3.common.vec_env import unwrap_vec_normalize
 
-p.connect(p.GUI)
-p.setAdditionalSearchPath(pybullet_data.getDataPath())
-planeId = p.loadURDF("plane.urdf")
-p.setGravity(0, 0, -10)
-angle = p.addUserDebugParameter('Steering', -0.5, 0.5, 0)
-throttle = p.addUserDebugParameter('Throttle', 0, 20, 0)
+env = gym.make('AntBulletEnv-v0')
 
-car = p.loadURDF('simplecar.urdf', [0, 0, 0.1])
-sleep(3)
-wheel_indices = [1, 3, 4, 5]
-hinge_indices = [0, 2]
-
-while True:
-    user_angle = p.readUserDebugParameter(angle)
-    user_throttle = p.readUserDebugParameter(throttle)
-    for joint_index in wheel_indices:
-        p.setJointMotorControl2(car, joint_index,
-                                p.VELOCITY_CONTROL,
-                                targetVelocity=user_throttle)
-    for joint_index in hinge_indices:
-        p.setJointMotorControl2(car, joint_index,
-                                p.POSITION_CONTROL,
-                                targetPosition=user_angle)
-    p.stepSimulation()
+env.render()  # call this before env.reset, if you want a window showing the environment
+obs = env.reset()
+for i in range(10000):
+    action = env.action_space.sample()
+    obs, reward, done, info = env.step(action)
+    print(obs)
+    env.render()
+    if done:
+        obs = env.reset()
