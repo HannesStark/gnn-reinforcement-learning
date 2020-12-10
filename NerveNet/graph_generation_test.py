b5b07c6eca1f31b61412c4d67f4b3f292e8d3074
Tobias Schmidt <tsbaunatal@gmail.com>
2020-12-10 16:45:33 +0100

    more graph visualizations

--- NerveNet/graph_generation_test.py
+++ NerveNet/graph_generation_test.py
@@ -1,21 +1,39 @@
-from graph_util.mujoco_parser import parse_mujoco_graph
+from graph_util.mujoco_parser import parse_mujoco_graph, OB_MAP, XML_DICT
 import json
 from pathlib import Path
 
-task_name = "AntS-v1"
-dump_dir = Path("graph_logs")
-dump_dir.mkdir(parents=True, exist_ok=True)
-graph = parse_mujoco_graph(task_name=task_name)
 
-# make bs4.element.Tag type of xml document serializable
-for i in range(len(graph["tree"])):
-    graph["tree"][i]["raw"] = str(graph["tree"][i]["raw"])
+def generate_graph_log(task_name: str):
+    assert task_name in OB_MAP.keys(
+    ), f"{task_name} is not yet supported, try one of the following: " + str(OB_MAP.keys())
+
+    dump_dir = Path("graph_logs")
+    dump_dir.mkdir(parents=True, exist_ok=True)
+
+    nervenet_dir = Path("C:\\Users\\tsbau\\git\\NerveNet")
+
+    graph = parse_mujoco_graph(task_name=task_name, xml_assets_path=(
+        nervenet_dir / "environments" / "assets"))
+
+    # make bs4.element.Tag type of xml document serializable
+    for i in range(len(graph["tree"])):
+        graph["tree"][i]["raw"] = str(graph["tree"][i]["raw"])
+        if "attached_joint_info" in graph["tree"][i]:
+            for j in range(len(graph["tree"][i]["attached_joint_info"])):
+                graph["tree"][i]["attached_joint_info"][j]["raw"] = str(
+                    graph["tree"][i]["attached_joint_info"][j]["raw"])
+
+    # make numpy array serializable
+    graph['relation_matrix'] = graph['relation_matrix'].tolist()
+    for key in graph['node_parameters'].keys():
+        graph['node_parameters'][key] = graph['node_parameters'][key].tolist()
+
+    with open(str(dump_dir / f"{task_name}.json"), 'w') as f:
+        json.dump(graph, f, indent=4)
 
 
-# make numpy array serializable
-graph['relation_matrix'] = graph['relation_matrix'].tolist()
-for key in graph['node_parameters'].keys():
-    graph['node_parameters'][key] = graph['node_parameters'][key].tolist()
-
-with open(str(dump_dir / f"{task_name}.json"), 'w') as f:
-    json.dump(graph, f, indent=4)
+for task_name in XML_DICT.keys():
+    try:
+        generate_graph_log(task_name)
+    except:
+        print(f"Could not generate graph log for {task_name}.")
