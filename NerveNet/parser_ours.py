78e45ec9456b3e0ba49bb1199d7de1adfe600576
Tobias Schmidt <tsbaunatal@gmail.com>
2020-12-22 14:29:39 +0100

    WIP observation input mapping

--- NerveNet/parser_ours.py
+++ NerveNet/parser_ours.py
@@ -8,6 +8,7 @@ from graph_util.mujoco_parser_nervenet import XML_DICT as NERVENET_XML_DICT
 nervenet_assets_dir = Path(os.getcwd()).parent / \
     "NerveNet" / "environments" / "assets"
 
+generate_graph_log("AntBulletEnv-v0")
 generate_graph_log("AntS-v1",
                    xml_assets_path=nervenet_assets_dir)
 
