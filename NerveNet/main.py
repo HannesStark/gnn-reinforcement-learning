a710c78be1253fb8b96605d9d65e76d293273595
Tobias Schmidt <tsbaunatal@gmail.com>
2021-01-07 15:54:30 +0000

    local training and ngrok

--- NerveNet/main.py
+++ NerveNet/main.py
@@ -3,6 +3,11 @@ import os
 from datetime import datetime
 from pathlib import Path
 
+import torch
+from torch import nn
+from torch_geometric.nn import GCNConv
+from NerveNet.models.nerve_net_conv import NerveNetConv
+
 import gym
 from stable_baselines3 import PPO, A2C
 from stable_baselines3.common.callbacks import CheckpointCallback
@@ -30,21 +35,43 @@ nervenet_assets_dir = Path(os.getcwd()).parent / \
 env = gym.make(task_name)
 
 log_name = '{}_{}'.format(task_name, datetime.now().strftime('%d-%m_%H-%M-%S'))
-checkpoint_callback = CheckpointCallback(save_freq=1000, save_path='runs/' + log_name,
+checkpoint_callback = CheckpointCallback(save_freq=50, save_path='runs/' + log_name,
                                          name_prefix='rl_model')
 model = A2C("GnnPolicy",
             env,
-            # reducing batch_size to 1
-            n_steps=8,
             verbose=1,
             policy_kwargs={
                 'mlp_extractor_kwargs': {
                     'task_name': task_name,
-                    'xml_assets_path': None
-                }
+                    'xml_assets_path': None,
+                },
+                'net_arch':  {
+                    "input": [
+                        (nn.Linear, 12),
+                        (nn.Linear, 16)
+                    ],
+                    "propagate": [
+                        (NerveNetConv, 12),
+                        (NerveNetConv, 12),
+                        (NerveNetConv, 12)
+                    ],
+                    "policy": [
+                        (nn.Linear, 16),
+                        (nn.Linear, 16)
+                    ],
+                    "value": [
+                        (nn.Linear, 16),
+                        (nn.Linear, 16)
+                    ]
+                },
+                "activation_fn":  nn.Tanh,
             },
-            tensorboard_log="runs")
+            tensorboard_log="runs",
+            learning_rate=3.0e-4,
+            # batch_size=64,
+            # n_epochs=10,
+            n_steps=1024)
 
-model.learn(total_timesteps=10000, callback=checkpoint_callback,
+model.learn(total_timesteps=1000000, callback=checkpoint_callback,
             tb_log_name=log_name)
 model.save("a2c_ant")
