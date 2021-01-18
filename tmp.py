819a19dcb4e651c252a68b06e57327067d1974e4
Hannes Stärk <hannes.staerk@gmail.com>
2021-01-18 08:17:11 +0100

    more steps

--- tmp.py
+++ tmp.py
@@ -8,13 +8,14 @@ import numpy as np
 sns.set_theme()
 
 input_paths = [
-    'Mlp_split_architecture_AntBulletEnv-v0_13-01_08-02-20'
+    'Mlp_AntBulletEnv-v0_10-01_09-58-23',
+    'antgnn'
 ]
 
 lists = []
 min_len = np.inf
 for path in input_paths:
-    with open(os.path.join('runs', path, 'rollout_mean_every_step.txt')) as file:
+    with open(os.path.join('runs', path, 'rollout_mean.txt')) as file:
         lines = file.readlines()
         rewards = []
         for line in lines:
@@ -27,18 +28,19 @@ for reward_list in lists:
     arrays.append(np.array(reward_list)[:min_len])
 
 # take the mean
-window_size= 10000
+window_size= 100
 for i, array in enumerate(arrays):
     arrays[i] = np.convolve(array,np.ones(window_size)/window_size,mode='valid')
 
-plt.rcParams['figure.figsize'] = [10, 8]
+plt.rcParams['figure.dpi'] = 300
 array = np.array(arrays).T
 df = pd.DataFrame(array, columns=range(len(arrays)))
 
 sns.lineplot(data=df)
 plt.xlabel('1000 number of steps')
 plt.ylabel('mean reward in rollout')
-plt.legend(labels=['MLP', 'NerveNet-MLP'])
-# plt.savefig('figure.png')
+plt.title('Ant Environment')
+plt.legend(labels=['MLP', 'GNN'])
+plt.savefig('ant.png')
 plt.show()
 plt.clf()
