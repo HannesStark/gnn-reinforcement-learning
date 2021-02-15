bc726cabfc1bab0fab4de13c0fa559c18dcd6b5e
Hannes Stärk <hannes.staerk@gmail.com>
2021-02-15 08:22:45 +0100

    gnn for values experiment

--- train.py
+++ train.py
@@ -54,9 +54,11 @@ def train(args):
                 (NerveNetConv, 64),
             ],
             "policy": [
+                (nn.Linear, 64),
                 (nn.Linear, 16),
             ],
             "value": [
+                (nn.Linear, 64),
                 (nn.Linear, 16),
 
             ]
@@ -120,7 +122,7 @@ def train(args):
 def parse_arguments():
     p = argparse.ArgumentParser()
     p.add_argument('--config', type=argparse.FileType(mode='r'),
-                   default='configs/GNN_HalfCheetahBulletEnv-v0.yaml')
+                   default='configs/GNN_AntBulletEnv-v0.yaml')
     p.add_argument('--task_name', help='The name of the environment to use')
     p.add_argument(
         '--alg', help='The algorithm to be used for training', choices=["A2C", "PPO"])
